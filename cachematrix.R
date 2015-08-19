063466991a2b6dca0f05d2997b8be73078d59f6a
Courtney Hauth <chauth@charter.net>
2015-08-19 18:49:47 -0400

    Added additional comments and clarified some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,12 @@
-## Sometimes computing the inverse of a matrix takes a long time.  If the contents of the matrix does not change, it makes sense to cache
-## the inverse matrix so that wen we need it again.  We look up the cached value rather than recompute.  These functions setup the caching
-## of the inverse matrix.
+## The next two functions setup caching inverted matrix
+## The 'makeCacheMatrix' function defines how to make a cached matrix
+## The 'cacheSolve' function actually checks to see if the inverted matrix has been previously calculated and stored.  If so, it will return the stored inverted matrix.
+## If not, it will calculate, store and return the inverted matrix.
 
 ## The following function defines how to make a cached matrix.  There are 4 functions with the 'makeCacheMatrix' function.  
-## The first being 'set' which sets the value of the matrix.  Teh second function, 'get', gets the matrix.
-## The third function, 'setInverse', set the inverse of the matrix.  The fourth function, 'getInverse', gets the cached inverse matrix.
-## The last 'list' function just creates a list of functions so they can be called.
+## The first being 'set' which sets the value of the matrix.  The second function, 'get', gets the matrix.
+## The third function, 'setInverse', set the inverse of the matrix.  The fourth function, 'getInverse', gets the cached inversed matrix.
+## The 'list' function just creates a list of functions so they can be called.
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -21,7 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## This function will check to see if the inverse matrix is cached.  If so, return the cached inverted matrix.
-## If not, compute the inverse and store the results for next time.
+## If not, compute the inverse matrix and store the results.  Next time the cached value is returned instead of recalculating again.
 
 cacheSolve <- function(x, ...) {
     m <- x$getInverse()
